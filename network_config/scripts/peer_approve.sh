05136ae6b7e772e102b5a58d4dcb84166cade954
Maria V <mariatua2@gmail.com>
2023-02-08 13:43:41 -0400

    test that you can install and access chaincode in test channel

---
+++ network_config/scripts/peer_approve.sh
@@ -0,0 +1,14 @@
+export PATH=${PWD}/../bin:$PATH
+export FABRIC_CFG_PATH=$PWD/../config/
+# this id must be changed always to the result of peer lifecycle chaincode queryinstalled
+export CC_PACKAGE_ID=basic_0.1:e1d109599a40a8ebedc1c00092c898383e28685d93bf013c8207535f5d3f6731
+
+export CORE_PEER_TLS_ENABLED=true
+export CORE_PEER_LOCALMSPID="Org1MSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+export CORE_PEER_ADDRESS=localhost:7051
+
+peer lifecycle chaincode approveformyorg -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --channelID mychannel --name basic --version 0.1 --package-id $CC_PACKAGE_ID --sequence 1 --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem"
+
+peer lifecycle chaincode checkcommitreadiness --channelID mychannel --name basic --version 0.1 --sequence 1 --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" --output json

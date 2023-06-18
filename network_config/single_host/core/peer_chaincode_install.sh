d9e85b77c306efaf2ce012bdfda232cc01e5411d
Maria V <mariatua2@gmail.com>
2023-06-18 17:17:52 -0400

    Installing and approving chaincode on peers

--- network_config/single_host/core/peer_chaincode_install.sh
+++ network_config/single_host/core/peer_chaincode_install.sh
@@ -8,9 +8,11 @@ CC_NAME="$1"
 
 for PEER in {0..3}
 do
-## Install on P0
+## Install on peers
 infoln "Installing chaincode on peer $PEER"
 setPeerVars $PEER
 peer lifecycle chaincode install ${CC_NAME}.tar.gz
 done
 
+
+## This has to be different for each peer because we are not going to have the same chaincode for everything

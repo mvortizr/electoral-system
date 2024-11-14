bc942f30827e7ae128a3b0117213474b2b6cd9d9
maria veronica <mariatua2@gmail.com>
2024-11-14 17:46:27 -0400

    WIP

--- api/channel2/src/modules/vote/vote.service.ts
+++ api/channel2/src/modules/vote/vote.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class VoteService {}
+export class VoteService {
+    async postData(url: string, data: any): Promise<any> {
+        const response = await fetch(url, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(data)
+        });      
+      
+        return response.json();  
+      
+      }
+}
